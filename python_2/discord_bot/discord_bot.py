13ffe7749831dedd0cce7a8210fa168a61febe4a
romanrocks4 <roman.shubin27@gmail.com>
2025-06-25 13:48:27 -0400

    Added Google Gemini api and integrated it into the bot. It can take input and give responses within discord.

--- python_2/discord_bot/discord_bot.py
+++ python_2/discord_bot/discord_bot.py
@@ -1,13 +1,15 @@
 # Import the libraries
+from google import genai
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import random
 
-# Load the .env file
+# Load the .env file with the Discord token and AI api key
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+api_key = os.getenv("apikey")
 
 # Setup intents
 intents = discord.Intents.default()
@@ -26,6 +28,32 @@ async def on_ready():
 async def send_dice(ctx):
     dice = random.randint(1,6)
     await ctx.send(f'You rolled a {dice} !')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return  # Prevent the bot from replying to itself
+    
+    # Ignore if the message starts with the command prefix
+    if message.content.startswith(bot.command_prefix):
+        await bot.process_commands(message)
+        return
+    
+     #  Store the message in a variable
+    contents = message.content
+
+    # gives the api key to 
+    client = genai.Client(api_key = api_key)
+
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents= message.content,
+)
+
+    if message.content.lower():
+        await message.channel.send(response.text)
+
+    await bot.process_commands(message)  # IMPORTANT if you're using commands
 
 
 # Start the bot
